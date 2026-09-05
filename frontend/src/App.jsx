fe540ef5928f110fef3940590f6fb42b10f2f424
Jaden Randolph <JadenRandolph@outlook.com>
2026-09-05 00:16:19 -0400

    changing page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import ImageProcessingPage from './components/ImageProcessingPage';
 
 const App = () => {
   return (
-    <div className="bg-gray-900 min-h-screen flex flex-col items-center py-25">
+    <div className="bg-gray-900 min-h-screen flex flex-col items-center py-15">
       <header>
         <h1 className="text-4xl font-bold text-green-500">Do You Mog?</h1>
       </header>
--- frontend/src/components/ImageProcessingPage.jsx
+++ frontend/src/components/ImageProcessingPage.jsx
@@ -26,36 +26,38 @@ const ImageProcessingPage = () => {
 
     return (
         <div>
-            <h2 className="text-2xl font-semibold text-green-500">Upload Your Image</h2>
-            <div>
-                {/*if a file has been uploaded display it here */}
-                {selectedFile && (
-                    <div className="mt-4">
-                        <img
-                            src={URL.createObjectURL(selectedFile)}
-                            alt="Uploaded Image"
-                            className="mt-2 w-75 h-100 object-cover rounded-lg"
-                        />
-                    </div>
-                )}
-            </div>
-            <div>
-                {analysis && (
-                    <div>
-                        <h3 className="text-xl font-semibold text-green-500">Analysis Result:</h3>
-                        <p className="text-lg font-medium text-gray-300">{analysis.funny_comment}</p>
-                        <p className="text-lg font-medium text-gray-300">Mogging Score: {analysis.mog_probability}</p>
-                        <h4 className="text-lg font-medium text-gray-300">Most Mogging Features:</h4>
-                        <ul className="list-disc list-inside text-gray-300">
-                            {Object.entries(analysis.contributions).map(([feature, contribution]) => (
-                                <li key={feature} className="text-gray-300"> {feature}: {contribution} </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+            {/*two columns setup, left for uploaded image and right for the analysis*/}
+            <div className="flex flex-row">
+                <div className="flex flex-col items-center">
+                    {/*if a file has been uploaded display it here */}
+                    {selectedFile && (
+                        <div className="mt-4">
+                            <img
+                                src={URL.createObjectURL(selectedFile)}
+                                alt="Uploaded Image"
+                                className="mt-2 w-75 h-100 object-cover rounded-lg"
+                            />
+                        </div>
+                    )}
+                </div>
+                <div className="flex flex-col items-center px-10 py-5">
+                    {analysis && (
+                        <div>
+                            <h3 className="text-xl font-semibold text-green-500">Analysis Result:</h3>
+                            <p className="text-lg font-medium text-gray-300">{analysis.funny_comment}</p>
+                            <p className="text-lg font-medium text-gray-300">Mogging Score: {analysis.mog_probability}</p>
+                            <h4 className="text-lg font-medium text-gray-300">Most Mogging Features (Out of 10 points):</h4>
+                            <ul className="list-disc list-inside text-gray-300">
+                                {Object.entries(analysis.contributions).map(([feature, contribution]) => (
+                                    <li key={feature} className="text-gray-300"> {feature}: {contribution} </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
             </div>
             <form onSubmit={(e) => { e.preventDefault(); handleSubmit(); }}>
-                <input type="file" accept="image/*" onChange={handleFileChange} className="p-2 border border-gray-500 rounded mb-4 text-gray-300" />
+                <input type="file" accept="image/*" onChange={handleFileChange} className="p-2 border border-gray-500 rounded mb-4 text-gray-300 cursor-pointer" />
                 <button type="submit" disabled={!selectedFile} className="mt-4 px-4 py-2 bg-green-500 text-white font-semibold rounded hover:bg-green-600">Analyze Your Mogging</button>
             </form>
         </div>
